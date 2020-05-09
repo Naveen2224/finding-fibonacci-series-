2900c1b8c15bcc4599d51c436ff145170f1af543
Naveen2224 <65078185+Naveen2224@users.noreply.github.com>
2020-05-09 20:50:22 +0530

    Created fibonacci.py
    Fibonacci series is printed

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+n=int(input("enter the no.of numbers in series: "))
+a=0
+b=1
+if n<=0:
+    print('not possible')
+elif n==1:
+    print(a)
+elif n>=2:
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c=a+b
+        a=b
+        b=c
+        print(c)
